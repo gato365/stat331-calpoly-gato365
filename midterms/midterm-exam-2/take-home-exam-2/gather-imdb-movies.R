d3f7cc223f794e477cd86da7d8d6c210dc4db16f
gato365 <immanuelwilliams@gato365.com>
2024-03-02 08:33:20 -0800

    Added image of schema

--- midterms/midterm-exam-2/take-home-exam-2/gather-imdb-movies.R
+++ midterms/midterm-exam-2/take-home-exam-2/gather-imdb-movies.R
@@ -1,7 +1,7 @@
 table_names <- c("actors", "directors", "directors_genres",
                  "movies", "movies_directors", "movies_genres", "roles")
 
-url_base <- "https://raw.githubusercontent.com/gato365/stat331-calpoly-gato365/master/midterms/midterm-exam-2/take-home-exam-2/data/movie-db"
+url_base <- "https://raw.githubusercontent.com/gato365/stat331-calpoly-gato365/master/midterms/midterm-exam-2/take-home-exam-2/data/movie-db/"
 
 # For each table name, read the tables in and store them as the name of the table
 purrr::walk(table_names, function(x) {
