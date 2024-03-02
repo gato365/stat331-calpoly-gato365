d2aa2e2137ebe538529ba1c36e6f8ceab4a59d9e
gato365 <immanuelwilliams@gato365.com>
2024-03-02 07:05:41 -0800

    Changing

---
+++ midterms/midterm-exam-2/take-home-exam-2/gather-imdb-movies.R
@@ -0,0 +1,9 @@
+table_names <- c("actors", "directors", "directors_genres",
+                 "movies", "movies_directors", "movies_genres", "roles")
+
+url_base <- "https://raw.githubusercontent.com/gato365/stat331-calpoly-gato365/master/midterms/midterm-exam-2/take-home-exam-2/data/movie-db"
+
+# For each table name, read the tables in and store them as the name of the table
+purrr::walk(table_names, function(x) {
+  assign(x, readr::read_csv(paste0(url_base, x, ".csv")), envir = .GlobalEnv)
+})
